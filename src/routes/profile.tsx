18626c096c6e1098efc6307d502c48775aa82ba4
leesaewa <saewa0424@gmail.com>
2024-03-22 16:55:35 +0900

    profile 코드 정리

--- src/routes/profile.tsx
+++ src/routes/profile.tsx
@@ -124,7 +124,6 @@ export default function Profile() {
   const onEditCancel = () => {
     setEdit(false);
     setEditName(user?.displayName ?? "");
-    // setAvatar(null);
     setAvatar(user?.photoURL);
   };
 
@@ -165,17 +164,13 @@ export default function Profile() {
     <Wrapper>
       <EditContainer>
         <ButtonWrapper>
-          {edit ? (
-            <>
-              <EditBtn className="btn-save" onClick={onEditSave}>
-                Save
-              </EditBtn>
-              <EditBtn className="btn-cancel" onClick={onEditCancel}>
-                Cancel
-              </EditBtn>
-            </>
-          ) : (
-            <EditBtn onClick={onEdit}>Edit</EditBtn>
+          <EditBtn className="btn-save" onClick={edit ? onEditSave : onEdit}>
+            {edit ? "Save" : "Edit"}
+          </EditBtn>
+          {edit && (
+            <EditBtn className="btn-cancel" onClick={onEditCancel}>
+              Cancel
+            </EditBtn>
           )}
         </ButtonWrapper>
 
@@ -211,7 +206,22 @@ export default function Profile() {
           </>
         ) : (
           <>
-            <AvatarImg src={avatar} />
+            {avatar ? (
+              <AvatarImg src={avatar} />
+            ) : (
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                viewBox="0 0 24 24"
+                fill="currentColor"
+                className="w-6 h-6"
+              >
+                <path
+                  fillRule="evenodd"
+                  d="M7.5 6a4.5 4.5 0 1 1 9 0 4.5 4.5 0 0 1-9 0ZM3.751 20.105a8.25 8.25 0 0 1 16.498 0 .75.75 0 0 1-.437.695A18.683 18.683 0 0 1 12 22.5c-2.786 0-5.433-.608-7.812-1.7a.75.75 0 0 1-.437-.695Z"
+                  clipRule="evenodd"
+                />
+              </svg>
+            )}
             <Name>{user?.displayName ?? "Anonymous"}</Name>
           </>
         )}
